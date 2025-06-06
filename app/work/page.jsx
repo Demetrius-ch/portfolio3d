28e7b55b915710642b06f3322e48b572fbfa4cea
Demetrius-ch <lgdemetrius@gmail.com>
2025-06-07 00:58:05 +0100

    Ajustement des polices

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,8 +1,32 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React, {useState} from "react";
-import {Swiper, SwiperSlide} from 'swiper/react'
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../../components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+import { title } from "process";
+
+const project = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, {name: "JavaScript"}],
+    
+  },
+];
 const Work = () => {
   return <div> work page</div>;
 };
